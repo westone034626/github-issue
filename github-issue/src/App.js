8e1f00f3f2824cbb7562d07b73e83e9779bc8419
서원석 <seows@laivdata.com>
2021-04-30 09:36:55 +0900

    [Add] code calling hook&displaying label on App.js

--- github-issue/src/App.js
+++ github-issue/src/App.js
@@ -3,8 +3,15 @@ import Header from './components/Header';
 import Button from './components/Button';
 import Label from './components/Label';
 import LabelItem from './components/LabelItem';
+import { useToggle, useFetchData } from './hooks';
 
 function App() {
+  const [currentTab, setCurrentTab] = useToggle('labels');
+  const REQUEST_URL = `http://localhost:3001/${currentTab}`;
+  const [data, isLoading] = useFetchData(REQUEST_URL);
+  const onBtnClick = (value) => {
+    setCurrentTab(value);
+  };
   return (
     <div className="App">
       <Header message="ISSUES" iconName="book" />
@@ -13,23 +20,35 @@ function App() {
         fontColor="white"
         buttonColor="blue"
         iconName="label"
+        onClick={() => {
+          onBtnClick(currentTab);
+        }}
       />
       <Button
         message="Milestones"
         fontColor="black"
         buttonColor="white"
         iconName="mileStone"
+        onClick={() => {
+          onBtnClick(currentTab);
+        }}
       />
       <Button message="New label" fontColor="white" buttonColor="green" />
-      <LabelItem message="hello world">
-        <Label message="bug" fontColor="white" buttonColor="red" />
-      </LabelItem>
-      <LabelItem message="This is test LabelItem">
-        <Label message="warning" fontColor="white" buttonColor="red" />
-      </LabelItem>
-      <LabelItem message="This is test LabelItem">
-        <Label message="warning" fontColor="white" buttonColor="red" />
-      </LabelItem>
+      {isLoading ? (
+        <LabelItem message="loading..." /> // Would implement a loading component later.
+      ) : (
+        data.map((item) => (
+          <>
+            <LabelItem message={item.description}>
+              <Label
+                message={item.name}
+                fontColor="white"
+                buttonColor={item.color}
+              />
+            </LabelItem>
+          </>
+        ))
+      )}
     </div>
   );
 }
