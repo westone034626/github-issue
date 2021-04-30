79652b00da4fb257b1f4e6d3233d304fd3234fb4
서원석 <seows@laivdata.com>
2021-04-30 09:34:42 +0900

    [Add] hooks fetching data using by url info & etc.

---
+++ github-issue/src/hooks/index.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+export const useToggle = (initialState) => {
+  const [state, setState] = useState(initialState);
+  const toggle = () => {
+    setState(state === 'labels' ? 'mileStones' : 'labels');
+  };
+  return [state, toggle];
+};
+
+export const useFetchData = (url) => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+        })
+        .finally(() => setIsLoading(false));
+    }, 1000);
+  }, [url]);
+  return [data, isLoading];
+};
