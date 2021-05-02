a2db2ae2badc862705fa65b6313199fc8ec4d92d
westone034626 <dydrlaks@likelion.org>
2021-05-02 18:51:47 +0900

    [Add] NewLabelWrapper component

---
+++ github-issue/src/components/NewLabel.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const NewLabelWrapper = styled.div`
+  display: flex;
+  margin-bottom: 25px;
+  width: 100%;
+  height: 225px;
+  background-color: black;
+`;
+
+const NewLabel = () => {
+  return <NewLabelWrapper></NewLabelWrapper>;
+};
+
+export default NewLabel;
