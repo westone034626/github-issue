c0a63b0a58755ab27169925bd584677eb94b355f
westone034626 <dydrlaks@likelion.org>
2021-05-02 19:46:45 +0900

    [Edit] layout of NewLabel component

--- github-issue/src/components/NewLabel.js
+++ github-issue/src/components/NewLabel.js
@@ -1,15 +1,80 @@
 import styled from 'styled-components';
+import Label from './Label/Label';
 
 const NewLabelWrapper = styled.div`
   display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
   margin-bottom: 25px;
   width: 100%;
-  height: 225px;
-  background-color: black;
+
+  border: 1px solid black;
+  padding: 10px 10px 10px 10px;
 `;
 
 const NewLabel = () => {
-  return <NewLabelWrapper></NewLabelWrapper>;
+  return (
+    <NewLabelWrapper>
+      <Label />
+      <form style={{ width: '100%', marginTop: '25px' }}>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            alignItems: 'flex-end',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'flex-start',
+              flexDirection: 'column',
+              width: '25%',
+            }}
+          >
+            <p>label name</p>
+            <input style={{ width: '100%' }} />
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'flex-start',
+              flexDirection: 'column',
+              width: '25%',
+            }}
+          >
+            <p>description</p>
+            <input style={{ width: '100%' }} />
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'flex-start',
+              flexDirection: 'column',
+              width: '25%',
+            }}
+          >
+            <p>color</p>
+            <input style={{ width: '100%' }} />
+          </div>
+          <div>
+            <button>cancel</button>
+            <button
+              type="submit"
+              style={{ marginLeft: '5px' }}
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              create label
+            </button>
+          </div>
+        </div>
+      </form>
+    </NewLabelWrapper>
+  );
 };
 
 export default NewLabel;
