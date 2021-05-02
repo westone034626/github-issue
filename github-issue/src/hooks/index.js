a690d5cdff5bed12327a0bb79212282edef5adcd
westone034626 <dydrlaks@likelion.org>
2021-05-02 18:40:11 +0900

    [Add] custom hook named useToggle

--- github-issue/src/hooks/index.js
+++ github-issue/src/hooks/index.js
@@ -1,5 +1,13 @@
 import { useState, useEffect } from 'react';
 
+export const useToggle = (initialState = false) => {
+  const [state, setState] = useState(initialState);
+  const toggle = () => {
+    setState((prev) => !prev);
+  };
+  return [state, toggle];
+};
+
 export const useFetchData = (url) => {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
